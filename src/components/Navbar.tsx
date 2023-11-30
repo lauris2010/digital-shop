d28785afb7882fd49bb9ae9902e6ec4aa1bde5f0
lauris2010 <laurynas.martinkus2009@gmail.com>
2023-11-30 13:40:17 +0200

    Dropdown, logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,12 @@ import { Icons } from "./Icons";
 import NavItems from "./NavItems";
 import { buttonVariants } from "./ui/button";
 import Cart from "./Cart";
-
-const Navbar = () => {
-  const user = null;
+import { getServerSideUser } from "@/lib/payload-utils";
+import { cookies } from "next/headers";
+import UserAccountNav from "./UserAccountNav";
+const Navbar = async () => {
+  const nextCookies = cookies();
+  const { user } = await getServerSideUser(nextCookies);
 
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
@@ -24,7 +27,6 @@ const Navbar = () => {
               <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
                 <NavItems />
               </div>
-
               <div className="ml-auto flex items-center">
                 <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg-space-x-6">
                   {user ? null : (
@@ -42,7 +44,7 @@ const Navbar = () => {
                     ></span>
                   )}
                   {user ? (
-                    <p></p>
+                    <UserAccountNav user={user} />
                   ) : (
                     <Link
                       href="/sign-up"
